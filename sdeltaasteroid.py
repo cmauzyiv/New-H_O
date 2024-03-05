ffe4eb9ad4b949f41593d72632fd3d03ab6ce2fc
cmauzyiv <80534067+cmauzyiv@users.noreply.github.com>
2024-03-05 14:38:33 -0500

    Add files via upload

---
+++ sdeltaasteroid.py
@@ -0,0 +1,81 @@
+import pygame
+import constance as c
+from particle_spawner import ParticleSpawner
+import random
+from Explosions import P1AsteroidExplosions
+
+class SdeltaAsteroid(pygame.sprite.Sprite): ##this is how the ship will look and spawn
+    def __init__(self):
+        super(SdeltaAsteroid, self).__init__()
+        self.img_asteroid_01 = pygame.image.load('gem3_01.png').convert_alpha()
+        self.img_asteroid_02 = pygame.image.load('gem3_02.png').convert_alpha()
+        self.img_asteroid_03 = pygame.image.load('gem3_03.png').convert_alpha()
+        self.img_asteroid_04 = pygame.image.load('gem3_04.png').convert_alpha()
+        self.img_asteroid_05 = pygame.image.load('gem3_05.png').convert_alpha()
+        self.img_asteroid_06 = pygame.image.load('gem3_06.png').convert_alpha()
+        self.img_asteroid_07 = pygame.image.load('gem3_07.png').convert_alpha()
+        self.img_asteroid_08 = pygame.image.load('gem3_08.png').convert_alpha()
+        self.img_asteroid_09 = pygame.image.load('gem3_09.png').convert_alpha()
+        self.img_asteroid_10 = pygame.image.load('gem3_10.png').convert_alpha()
+
+
+        self.anim_asteroid = [self.img_asteroid_01, self.img_asteroid_02, self.img_asteroid_03,
+                              self.img_asteroid_04, self.img_asteroid_05, self.img_asteroid_06,
+                              self.img_asteroid_07, self.img_asteroid_08, self.img_asteroid_09,
+                              self.img_asteroid_10]
+
+        self.anim_index = 0
+        self.frame_length_max = 3
+        self.explosion_group = pygame.sprite.Group()
+        self.frame_length = self.frame_length_max
+        self.image = self.anim_asteroid[self.anim_index]
+        self.scale_value = random.uniform(1.0, 1.33)
+        self.image = pygame.transform.scale(self.image, (int(self.image.get_width() * self.scale_value),
+                                            int(self.image.get_height()* self.scale_value)))
+        self.rect = self.image.get_rect()
+        self.is_destroyed = False
+        self.is_invincible = False
+        self.rect.x = random.randrange(0, (c.display_width - self.rect.width))
+        self.rect.y = -self.rect.height
+        self.hp_timer = random.randrange(60, 1000)
+        self.hp = 0 ##this is how i will probably do reinforcement schedule. I set HP or IRT then down below, i will make it so that it will only blow up after meets irt
+        self.point_value = 1
+        self.lasers = pygame.sprite.Group()
+        self.snd_hit = pygame.mixer.Sound("asteroid_sound.ogg")
+        self.vel_x = 0
+        self.vel_y = 1 #how fast its moving down y axis
+        self.speed = 5
+
+    def update(self):
+        self.rect.x += self.vel_x
+        self.rect.y += self.vel_y
+        self.explosion_group.update()
+        max_index = len(self.anim_asteroid) - 1
+        if self.frame_length == 0:
+            self.anim_index += 1
+            if self.anim_index > max_index:
+                self.anim_index = 0
+            else:
+                self.image = self.anim_asteroid[self.anim_index]
+                self.frame_length = self.frame_length_max
+        else:
+            self.frame_length -= 1
+
+
+
+
+
+
+    def get_hit(self):
+        if not self.is_invincible:
+            ##self.snd_hit.play()
+            self.is_destroyed = True
+            new_explosion = P1AsteroidExplosions()
+            new_explosion.rect.x = self.rect.x + 16
+            new_explosion.rect.y = self.rect.y
+            self.explosion_group.add(new_explosion)
+            self.rect.x = self.rect.x
+            self.rect.y = self.rect.y
+            self.image = new_explosion.anim_explosion[new_explosion.anim_index]
+        else:
+            self.snd_hit.play()
